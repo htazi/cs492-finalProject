f482a4245c43cf33f32fc506bef590b9889fbece
hassan tazi <31481357+htazi@users.noreply.github.com>
2018-12-27 12:07:10 -0500

    Add files via upload

---
+++ questionVerification.php
@@ -0,0 +1,50 @@
+<?php
+
+
+//session_start(); 
+
+if (isset($_POST['submit'])){
+    
+    include 'dbh.php';
+    
+   // $secqt = mysqli_real_escape_string($conn, $_POST['secqt']);
+    $answer = mysqli_real_escape_string($conn, $_POST['questionanswer']);
+    
+    // Erroe handlers
+    // Check if inputs are empty
+    
+    if(empty ($answer)){
+         header("Location: ../cs492/questionVerification.html?login=empty");
+         exit ();
+        
+    }else{
+        $sql = "SELECT *FROM users WHERE user_answer1 = '$answer'";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+        if ($resultCheck <1 ){
+            header("Location: ../cs492/questionVerification.html?login=error");
+            exit ();
+            
+        }else{
+            if ($row = mysqli_fetch_assoc($result)) {
+               // if ($answer == false{
+                     header("Location: ../cs492/RSA.html?login=success");
+                     exit ();
+                }else{
+                    header("Location: ../cs492/questionVerification.html?login=error");
+                     exit ();
+                }
+            
+        }
+    }
+}else{
+    
+    header("Location: ../cs492/index.html?login=error");
+    exit ();
+}
+
+
+
+
+
+?>
